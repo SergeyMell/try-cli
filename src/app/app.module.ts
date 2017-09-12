b04efa480ee8f47b037c599df64b6d5d853aec22
SergeyMell <sergey.mell@agilie.com>
2017-09-12 18:59:33 +0300

    Implemented auth checking guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import {environment} from '../environments/environment';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {ChatComponent} from './chat/chat.component';
+import {RouterModule, Routes} from "@angular/router";
+import {UserSignedInGuard} from "./guards/usersignin.guard";
+
+const routes: Routes = [
+  {path: '', component: ChatComponent, canActivate: [UserSignedInGuard]}
+];
 
 @NgModule({
   declarations: [
@@ -14,11 +20,12 @@ import {ChatComponent} from './chat/chat.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule
   ],
-  providers: [],
+  providers: [UserSignedInGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/guards/usersignin.guard.ts
@@ -0,0 +1,21 @@
+import {Injectable} from "@angular/core";
+import {AngularFireAuth} from "angularfire2/auth";
+import {CanActivate} from "@angular/router";
+
+@Injectable()
+export class UserSignedInGuard implements CanActivate {
+
+  constructor(private af: AngularFireAuth) {
+
+  }
+
+  canActivate() {
+    return new Promise((resolve, reject) => {
+      this.af.authState.subscribe(
+        () => { resolve(true); },
+        reject
+      );
+    });
+  }
+
+}
