f24c4985898c7c9dd8cbb68cf3b4492c8189a529
SergeyMell <sergey.mell@agilie.com>
2017-09-12 18:08:27 +0300

    Fully implemented chat component with firebase integration

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,9 +1,20 @@
 import {Component} from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.template.html'
 })
 export class ChatComponent {
+
+  messages: FirebaseListObservable<any>;
+
+  constructor(db: AngularFireDatabase) {
+    this.messages = db.list('/messages');
+  }
+
+  addMessage(message) {
+    this.messages.push({message: message});
+  }
 
 }
