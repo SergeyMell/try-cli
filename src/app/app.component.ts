ac41ff45005b15334b3e72ca0895d00c4fc9679d
SergeyMell <sergey.mell@agilie.com>
2017-09-19 18:24:31 +0300

    Public messages functionality implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MdSnackBar} from '@angular/material';
 import * as io from 'socket.io-client';
 
@@ -10,12 +10,39 @@ import * as io from 'socket.io-client';
 export class AppComponent {
 
   publicChannel: any;
+  currentUser: any;
+  users = [];
+  messages = [];
 
   constructor(public snackBar: MdSnackBar) {
 
     this.publicChannel = io('https://soketer.herokuapp.com/');
     this.publicChannel.on('event', (data) => {
       this.snackBar.open(data['joke']);
+    });
+    this.publicChannel.on('user_join', (data) => {
+      this.users.push(data);
+    });
+    this.publicChannel.on('public_message', (data) => {
+      this.messages.push(data);
+    });
+  }
+
+  joinUser(name) {
+    const userObject = {
+      id: Date.now(),
+      name
+    };
+
+    // This could be simple Ajax request
+    this.publicChannel.emit('user_join', userObject);
+    this.currentUser = userObject;
+  }
+
+  sendMessage(message) {
+    this.publicChannel.emit('public_message', {
+      message,
+      user: this.currentUser
     });
   }
 
