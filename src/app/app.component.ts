a3fbc7d711c9b0619dad0b2e40e886605855fbd5
SergeyMell <sergey.mell@agilie.com>
2017-09-19 17:42:08 +0300

    Added Angular material to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MdSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(public snackBar: MdSnackBar) {
+    setTimeout(() => {
+      this.snackBar.open('Hello');
+    }, 1000);
+  }
 
 }
